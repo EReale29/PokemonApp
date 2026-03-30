07b3c7a425937897b8afc775a1f7a8eb862bdf1c
EReale29 <167756013+EReale29@users.noreply.github.com>
2026-03-30 22:09:20 +0000

    build fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import Navbar from "@/components/Navbar";
 import ErrorBoundary from "@/components/ErrorBoundary";
 import "@/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 
